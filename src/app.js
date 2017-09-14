fa8b74a22feed3bfe4cf831985d2530b8c84d0e4
Lukas Ochmann <lukas.ochmann@seitenbau.com>
2017-09-14 08:53:39 +0200

    create, write, and read File for start service

--- src/app.js
+++ src/app.js
@@ -1,3 +1,2 @@
 import write from './services/start'
-
 write("hallo")
--- src/services/start.js
+++ src/services/start.js
@@ -1,11 +1,29 @@
 import * as fs from 'fs';
-import { green, red } from 'chalk';
+import { green, red, blue } from 'chalk';
+
+function writeProcess(text) {
+  fs.readFile('./tmp/Times.txt', (err, file) => {
+    let data = text;
+    if(!err) {
+      data = `${file}\n${text}`;
+    }
+    fs.writeFile('./tmp/Times.txt', data, { flag: 'w'}, function(err) {
+      if(err) {
+        return console.log(red(err));
+      }
+      console.log(green("File saved!"));
+    });
+  })
+}
 
 export default function write(text) {
-  fs.writeFile('./tmp/Times.txt', text, function(err) {
-    if(err) {
-      return console.log(red(err));
+  fs.exists('./tmp/', (exists) => {
+    if (!exists) {
+      return fs.mkdir('./tmp/', () => {
+        console.log(blue('Temp dir Created!'));
+        writeProcess(text);
+      });
     }
-    console.log(green("File saved!"));
-  })
+    return writeProcess(text);
+  });
 }
